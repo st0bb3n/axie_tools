e07a71f4c2332ad2d6d6d03ab659bc97b716bb13
st0bb3n <52521318+st0bb3n@users.noreply.github.com>
2022-02-11 17:38:06 +0800

    Update ronintracker.py

--- ronintracker.py
+++ ronintracker.py
@@ -3,8 +3,6 @@ from datetime import datetime
 
 API_URL = "https://game-api.axie.technology/api/v1/0x"
 
-address = str(input("Whats the ronin address?: "))
-
 def generate_url(ronin):
     if len(ronin) == 46:
         url = API_URL + ronin[6:]
@@ -12,22 +10,26 @@ def generate_url(ronin):
     else:
         dead = "https://game-api.axie.technology/api/v1/0x0"
         return dead
+    
+while True:
+    address = str(input("Whats the ronin address?: "))
+    data_url = generate_url(address)
+    response = get(data_url)
+    print("API Status: ", response.status_code)
+    data_dict = response.json()
 
-data_url = generate_url(address)
-response = get(data_url)
-data_dict = response.json()
+    if data_dict != {}:
+        print("------Account Details------")
+        print("Name: ", data_dict["name"])
+        print("MMR: ", data_dict["mmr"])
+        print("Rank: ", data_dict["rank"])
+        print("------SLP------")
+        print("Current: ", data_dict["in_game_slp"])
+        print("Total Claimed: ", data_dict["lifetime_slp"])
+        unix = data_dict["next_claim"]
+        unix2 = data_dict["last_claim"]
+        print("Last Claim: ", (datetime.utcfromtimestamp(unix2).strftime('%Y-%m-%d %H:%M:%S')))
+        print("Next Claim: ", (datetime.utcfromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S')))
 
-if data_dict != {}:
-    print("Account Details")
-    print("Name: ", data_dict["name"])
-    print("MMR: ", data_dict["mmr"])
-    print("Rank: ", data_dict["rank"])
-    print("SLP")
-    print("Current: ", data_dict["in_game_slp"])
-    print("Total Claimed: ", data_dict["lifetime_slp"])
-    unix = data_dict["next_claim"]
-    print("Next Claim: ", (datetime.utcfromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S')))
-
-else:
-    print("Invalid Ronin Address")
-      
+    else:
+        print("Invalid Ronin Address")
